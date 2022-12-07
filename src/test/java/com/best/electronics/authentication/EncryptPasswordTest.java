3ac57ea318fc6705c4f7d3db8bc147bc7cde883b
Prabhleen Kaur <pr894105@dal.ca>
2022-12-07 01:39:51 -0400

    Fixed test case

--- src/test/java/com/best/electronics/authentication/EncryptPasswordTest.java
+++ src/test/java/com/best/electronics/authentication/EncryptPasswordTest.java
@@ -1,5 +1,6 @@
 package com.best.electronics.authentication;
 
+import com.best.electronics.login.EncryptPassword;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
