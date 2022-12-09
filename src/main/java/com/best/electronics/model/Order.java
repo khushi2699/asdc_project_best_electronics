14dbec6a95f55c303a5520a957e2ebd458dcec15
Prabhleen Kaur <pr894105@dal.ca>
2022-12-09 13:16:23 -0400

    Added changes for displaying order details of User

--- src/main/java/com/best/electronics/model/Order.java
+++ src/main/java/com/best/electronics/model/Order.java
@@ -1,8 +1,10 @@
 package com.best.electronics.model;
 
+import java.util.ArrayList;
+
 public class Order {
 
-    private String orderId;
+    private Integer orderId;
 
     private Double orderAmount;
 
@@ -12,4 +14,73 @@ public class Order {
 
     private String paymentMethod;
 
+    private User user;
+
+    private String Address;
+
+    private ArrayList<Product> products;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Double orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
 }
