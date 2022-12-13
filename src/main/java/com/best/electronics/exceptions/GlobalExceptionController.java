a6acbead934772a911358cec0853196d33c78d19
srishti <sr381365@dal.ca>
2022-12-12 22:06:04 -0400

    Fixed issues, refactored code.

--- src/main/java/com/best/electronics/exceptions/GlobalExceptionController.java
+++ src/main/java/com/best/electronics/exceptions/GlobalExceptionController.java
@@ -1,6 +1,4 @@
 package com.best.electronics.exceptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,17 +9,13 @@ public class GlobalExceptionController {
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<ErrorMessage> handleDataNotFoundException(DataNotFoundException ex) {
-        ErrorMessage message = new ErrorMessage();
-        message.getMessage();
-        message.setStatusCode(404);
-        return new ResponseEntity<ErrorMessage>(message, HttpStatus.NO_CONTENT);
+        ErrorMessage message = new ErrorMessage(404, ex.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.NO_CONTENT);
     }
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ErrorMessage> handleNullPointerException(NullPointerException ex) {
-        ErrorMessage message = new ErrorMessage();
-        message.getMessage();
-        message.setStatusCode(500);
-        return new ResponseEntity<ErrorMessage>(message, HttpStatus.NO_CONTENT);
+        ErrorMessage message = new ErrorMessage(404, ex.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.NO_CONTENT);
     }
 
 }
