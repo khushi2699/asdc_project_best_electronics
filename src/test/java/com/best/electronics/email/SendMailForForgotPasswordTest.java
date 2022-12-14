0df5fbbc938df6b8899508079f22e60778d8284c
Khushi <kh482021@dal.ca>
2022-12-14 18:03:34 -0400

    Adding test cases

--- src/test/java/com/best/electronics/email/SendMailForForgotPasswordTest.java
+++ src/test/java/com/best/electronics/email/SendMailForForgotPasswordTest.java
@@ -1,11 +1,15 @@
 package com.best.electronics.email;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.mail.MessagingException;
+
 @SpringBootTest
 public class SendMailForForgotPasswordTest {
+
     SendMailForForgotPassword sendMailForForgotPassword;
 
     @Before
@@ -14,8 +18,29 @@ public class SendMailForForgotPasswordTest {
     }
 
     @Test
-    public void draftEmailSuccessTest(){
-
+    public void draftEmailSuccessUserTest() throws MessagingException {
+        sendMailForForgotPassword.draftEmail(123,"user@gmail.com","User");
+        Assert.assertTrue(true);
     }
 
+    @Test
+    public void draftEmailSuccessFailureUserTest() throws MessagingException {
+        sendMailForForgotPassword.draftEmail(123,"null","User");
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void draftEmailSuccessAdminTest() throws MessagingException {
+        sendMailForForgotPassword.draftEmail(123,"admin@gmail.com","Admin");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void draftEmailSuccessFailureAdminTest() throws MessagingException {
+        sendMailForForgotPassword.draftEmail(123,"null","Admin");
+        Assert.assertFalse(false);
+    }
+
+
 }
+
