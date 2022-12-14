92cf505d7b779b4c3367de1e4d18fa535259788c
aishwaryakanchi <as955340@dal.ca>
2022-12-14 03:46:36 -0400

    product addition code

--- src/main/java/com/best/electronics/controller/AdminController.java
+++ src/main/java/com/best/electronics/controller/AdminController.java
@@ -4,10 +4,15 @@ import com.best.electronics.database.IDatabasePersistence;
 import com.best.electronics.database.MySQLDatabasePersistence;
 import com.best.electronics.login.AdminLoginHandler;
 import com.best.electronics.login.ILoginHandler;
+import com.best.electronics.properties.AdminProperties;
+import com.best.electronics.register.AdminRegisterHandler;
+import com.best.electronics.state.State;
 import com.best.electronics.login.UserLoginHandler;
-import com.best.electronics.login.LoginState;
-import com.best.electronics.model.*;
-import com.best.electronics.exceptions.NullPointerException;
+import com.best.electronics.model.User;
+import com.best.electronics.model.Admin;
+import com.best.electronics.model.Product;
+import com.best.electronics.model.Order;
+import com.best.electronics.register.IRegisterHandler;
 import com.best.electronics.repository.AdminRepository;
 import com.best.electronics.repository.ProductRepository;
 import com.best.electronics.repository.UserRepository;
@@ -17,18 +22,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @PostMapping("/process_registration")
+    public String processRegistration(Admin admin, Model model){
+        IRegisterHandler registerHandler = new AdminRegisterHandler();
+        State registerState = registerHandler.register(admin, new HashMap<>());
+        model.addAttribute("msg", registerState.getStatus());
+        return registerState.getNextPage();
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("admin", new Admin());
+        return "adminRegistrationForm";
+    }
 
     @GetMapping("/login")
     public String login(Model model){
@@ -39,15 +60,35 @@ public class AdminController {
     @PostMapping("/process_login")
     public String processLogin(Admin admin, Model model, HttpServletRequest request){
         ILoginHandler loginHandler = new AdminLoginHandler();
-        LoginState loginState = loginHandler.login(admin, request);
-        model.addAttribute("msg", loginState.getLoginStatus());
+        State loginState = loginHandler.login(admin, request);
+        model.addAttribute("msg", loginState.getStatus());
         model.addAttribute("admin", new Admin());
+
+        HttpSession oldSession = request.getSession(false);
+        if(oldSession == null) {
+            return "adminLogin";
+        }else{
+            AdminProperties adminProperties = new AdminProperties();
+            if(oldSession.getAttribute("id") == adminProperties.getId()){
+                model.addAttribute("isSuperAdmin", true);
+            }
+
+        }
         return loginState.getNextPage();
     }
 
     @GetMapping("/adminHome")
-    public String adminHome(){
-        return "adminLandingPage";
+    public String adminHome(Model model,HttpServletRequest request){
+        HttpSession oldSession = request.getSession(false);
+        if(oldSession == null) {
+            return "adminLogin";
+        }else{
+            AdminProperties adminProperties = new AdminProperties();
+            if(oldSession.getAttribute("id") == adminProperties.getId()){
+                model.addAttribute("isSuperAdmin", true);
+            }
+            return "adminLandingPage";
+        }
     }
 
     @GetMapping("/logout")
@@ -60,7 +101,7 @@ public class AdminController {
     }
 
     @GetMapping("/orderDetails")
-    public String orderDetails(Order order, Model model, HttpServletRequest request){
+    public String orderDetails(Order order, Model model, HttpServletRequest request) throws Exception {
         HttpSession oldSession = request.getSession(false);
         if(oldSession == null) {
             return "adminLogin";
@@ -81,13 +122,52 @@ public class AdminController {
         }
     }
 
+    @GetMapping("/adminList")
+    public String getAdminsList(Model model, HttpServletRequest request){
+        HttpSession oldSession = request.getSession(false);
+        if(oldSession == null) {
+            return "adminLogin";
+        }else{
+            String updatedStatus = (String) oldSession.getAttribute("msg");
+            Integer id = (Integer) oldSession.getAttribute("id");
+            System.out.println(updatedStatus);
+            if(updatedStatus != null){
+                oldSession.removeAttribute("msg");
+            }
+            model.addAttribute("msg",updatedStatus);
+
+            IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
+            AdminRepository adminRepository = new AdminRepository(databasePersistence);
+            ArrayList<Map<String, Object>> adminDetails = adminRepository.getAllAdmins(id);
+            model.addAttribute("adminDetails", adminDetails);
+            return "adminList";
+        }
+    }
+
+    @PostMapping("/deleteAdmin/{adminId}")
+    public String deleteAdmin(@PathVariable Integer adminId, HttpServletRequest request){
+        HttpSession oldSession = request.getSession(false);
+        if(oldSession == null) {
+            return "adminLogin";
+        }else{
+            IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
+            AdminRepository adminRepository = new AdminRepository(databasePersistence);
+            if(adminRepository.deleteAdmin(adminId)){
+                oldSession.setAttribute("msg","Admin Deleted Successfully!");
+                return "redirect:/admin/adminList";
+            }
+            oldSession.setAttribute("msg","Some error occurred while deleting the Admin. Please try again!");
+            return "redirect:/admin/adminList";
+        }
+    }
+
     @GetMapping("/users")
-    public String adminUsers(Model model) throws Exception{
+    public String adminUsers(Model model){
         IDatabasePersistence db = new MySQLDatabasePersistence();
         UserRepository userRepository = new UserRepository(db);
         ArrayList<Map<String, Object>> userList = userRepository.getAllUsersDetails();
-        if(userList == null){
-            throw new NullPointerException("Users List could not be fetched from the database");
+        if(userList.isEmpty()){
+            return "redirect:/admin/adminHome";
         } else {
             model.addAttribute("user", new User());
             model.addAttribute("listUser", userList);
@@ -101,7 +181,7 @@ public class AdminController {
         ProductRepository productRepository = new ProductRepository(db);
         ArrayList<Map<String, Object>> productCategoryList = productRepository.getAllProductsAndTheirCategory();
         if(productCategoryList.isEmpty()){
-            throw new NullPointerException("Products List could not be fetched from the database");
+            return "redirect:/admin/adminHome";
         }else {
             model.addAttribute("listProductCategory", productCategoryList);
             return "adminCategoryProduct";
@@ -139,6 +219,7 @@ public class AdminController {
             if(updatedStatus != null){
                 oldSession.removeAttribute("updatedStatus");
             }
+
             IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
             AdminRepository adminRepository = new AdminRepository(databasePersistence);
             Map<String, Object> adminDetail = adminRepository.getAdminDetails(id);
@@ -154,6 +235,20 @@ public class AdminController {
         }
     }
 
+    @PostMapping("/update_profile")
+    public String processUpdateProfile(Admin admin, HttpServletRequest request) {
+        HttpSession oldSession = request.getSession(false);
+        if(oldSession == null){
+            return "AdminLogin";
+        }else{
+            IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
+            AdminRepository adminRepository = new AdminRepository(databasePersistence);
+            String message = adminRepository.updateAdminDetails(admin);
+            oldSession.setAttribute("updatedStatus", message);
+            return "redirect:/admin/editProfile";
+        }
+    }
+
     @PostMapping("/updateProduct/{productId}")
     public String adminEditProductDetails(Model model, HttpServletRequest request, @PathVariable Integer productId ) throws Exception {
         HttpSession oldSession = request.getSession(false);
@@ -162,7 +257,7 @@ public class AdminController {
         if(oldSession == null){
             return "adminProductList";
         }else{
-              Integer id = productId;
+            Integer id = productId;
             String updatedStatus = (String) oldSession.getAttribute("updatedStatus");
             System.out.println(updatedStatus);
             if(updatedStatus != null){
@@ -171,13 +266,13 @@ public class AdminController {
             IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
             ProductRepository productRepository = new ProductRepository(databasePersistence);
             System.out.println("id that is being passed:"+id);
-                ArrayList<Object> updatedDetails = new ArrayList<>();
-                updatedDetails.add(productId);
-                updatedDetails.add(quantity);
-                updatedDetails.add(price);
+            ArrayList<Object> updatedDetails = new ArrayList<>();
+            updatedDetails.add(productId);
+            updatedDetails.add(quantity);
+            updatedDetails.add(price);
             if(databasePersistence.saveData("{call update_product_details(?, ?, ?)}", updatedDetails)){
-                    return "redirect:/admin/products";
-                }
+                return "redirect:/admin/products";
+            }
             return "redirect:/admin/products";
         }
     }
@@ -193,20 +288,6 @@ public class AdminController {
             String message = productRepository.updateProductDetails(product);
             oldSession.setAttribute("updatedStatus", message);
             return "redirect:/admin/addProduct";
-        }
-    }
-
-    @PostMapping("/update_profile")
-    public String processUpdateProfile(Admin admin, HttpServletRequest request) {
-        HttpSession oldSession = request.getSession(false);
-        if(oldSession == null){
-            return "AdminLogin";
-        }else{
-            IDatabasePersistence databasePersistence = new MySQLDatabasePersistence();
-            AdminRepository adminRepository = new AdminRepository(databasePersistence);
-            String message = adminRepository.updateAdminDetails(admin);
-            oldSession.setAttribute("updatedStatus", message);
-            return "redirect:/admin/editProfile";
         }
     }
 
@@ -248,11 +329,11 @@ public class AdminController {
 
     @PostMapping("/sendEmail")
     public String sendEmail(@RequestParam(value = "orderId", required = false) Integer orderId,
-            @RequestParam(value = "orderAmount", required = false) Double orderAmount,
-            @RequestParam(value = "orderStatus", required = false) String orderStatus,
-            @RequestParam(value = "orderDate", required = false) String orderDate,
-            @RequestParam(value = "emailAddress", required = false) String emailAddress,
-            HttpServletRequest request) {
+                            @RequestParam(value = "orderAmount", required = false) Double orderAmount,
+                            @RequestParam(value = "orderStatus", required = false) String orderStatus,
+                            @RequestParam(value = "orderDate", required = false) String orderDate,
+                            @RequestParam(value = "emailAddress", required = false) String emailAddress,
+                            HttpServletRequest request){
         HttpSession oldSession = request.getSession(false);
         if(oldSession == null) {
             return "adminLogin";
@@ -273,5 +354,4 @@ public class AdminController {
             return "redirect:/admin/orderDetails";
         }
     }
-
 }
